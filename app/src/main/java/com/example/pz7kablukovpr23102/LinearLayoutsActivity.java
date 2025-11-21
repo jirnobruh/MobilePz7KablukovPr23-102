df3b6b51ab824a631fe4bf090aa25c1471c691dc
Server\NOT/founD <purplefenix228@mail.ru>
2025-11-22 00:46:54 +0700

    Init proj. 2 pages created, but haves bugs

---
+++ app/src/main/java/com/example/pz7kablukovpr23102/LinearLayoutsActivity.java
@@ -0,0 +1,19 @@
+package com.example.pz7kablukovpr23102;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LinearLayoutsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear_layouts);
+    }
+}
